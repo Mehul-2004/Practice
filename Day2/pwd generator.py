b1b50a121eaa0fdbba311bc8e3ed2f2cdd5edc7f
Mehul <mehulladwa46@gmail.com>
2026-07-08 23:06:26 +0530

    Delete Day2 directory

--- Day2/pwd generator.py
+++
@@ -1,9 +0,0 @@
-import random
-
-characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
-
-PASSWORD = ""
-for i in range(8):
-    PASSWORD += random.choice(characters)
-
-print("Password Generated",PASSWORD)
