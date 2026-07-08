ae713da834fb47a701dde9c5ebdeae90b4aaa3de
Mehul <mehulladwa46@gmail.com>
2026-07-08 23:07:59 +0530

    Delete utils directory

--- utils/helper.py
+++
@@ -1,9 +0,0 @@
-from database.mysql_db import connect_db
-
-def get_connection():
-    connection = connect_db()
-    if connection is None:
-        return None, None
-    
-    return connection, connection.cursor()
-
